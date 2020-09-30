1a1bf470afb55dff8f1e7e7746d1737fd62414ca
Onder <onder.yilmz@gmail.com>
2020-09-30 20:43:53 +0300

    django multilanguage

---
+++ core/urls.py
@@ -0,0 +1,44 @@
+"""core URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+
+from django.utils.translation import gettext_lazy as _
+from lang.views import change_language
+
+
+urlpatterns = [
+    path('change_language/',
+         change_language,
+         name='change_language'),
+    path('i18n/', include('django.conf.urls.i18n')),
+
+]
+
+
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('', include('lang.urls')),
+    path(_('blog/'), include('blog.urls')),
+
+    # varsayılan dil için öneki gizler
+    prefix_default_language=False,
+
+)
+
+
